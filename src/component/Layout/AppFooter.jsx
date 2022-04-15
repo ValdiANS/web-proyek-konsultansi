8a448a69dbf63aa79fb2ef251c1dd7c846894b83
Rivaldi <rvdans002@gmail.com>
2022-04-15 21:39:34 +0700

    update AppFooter component

--- src/component/Layout/AppFooter.jsx
+++ src/component/Layout/AppFooter.jsx
@@ -1,13 +1,24 @@
+import { memo } from 'react';
 import { createPortal } from 'react-dom';
 
 const AppFooter = () => {
   const footerContent = (
-    <div className="px-4 py-6 border-0.5 border-textPrimary border-solid">
-      Footer Content
+    <div className="px-4 py-16 bg-primary flex flex-row justify-center">
+      <div className="maps">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126748.60912418492!2d107.5731166770536!3d-6.903273917378882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1650031643605!5m2!1sen!2sid"
+          width="600"
+          height="350"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="border-0"
+        ></iframe>
+      </div>
     </div>
   );
 
   return createPortal(footerContent, document.getElementById('footer'));
 };
 
-export default AppFooter;
+export default memo(AppFooter);
