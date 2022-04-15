b8644f8706dfdfc6a6813e3ee4d4be5b74fa518d
Rivaldi <rvdans002@gmail.com>
2022-04-15 21:37:14 +0700

    add SlideControllerButton component

---
+++ src/component/Hero/Swiper/SlideControllerButton.jsx
@@ -0,0 +1,17 @@
+import { useSwiper } from 'swiper/react';
+
+const SlideControllerButton = ({ idx, isActive = false }) => {
+  const swiper = useSwiper();
+
+  const btnClass = `w-12 h-[5px] cursor-pointer rounded-none duration-500 ${
+    isActive ? 'bg-orange' : 'bg-primary'
+  }`;
+
+  const clickHandler = () => {
+    swiper.slideTo(idx);
+  };
+
+  return <button onClick={clickHandler} className={btnClass}></button>;
+};
+
+export default SlideControllerButton;
