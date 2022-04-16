e7969a70482c30d17a2deea2cbf8012a1c67b37f
Rivaldi <rvdans002@gmail.com>
2022-04-16 20:11:07 +0700

    create BestSellerSection component

---
+++ src/component/Pages/Home/BestSellerSection.jsx
@@ -0,0 +1,54 @@
+import { XyzTransitionGroup } from '@animxyz/react';
+import ProductItemCard from '../../ProductItemCard/ProductItemCard';
+
+const BestSellerSection = ({ className = '' }) => {
+  return (
+    <section className={`container mx-auto ${className}`}>
+      <h1 className="font-light text-4xl mb-7">
+        Sort by <strong className="font-bold">Terlaris</strong>
+      </h1>
+
+      <div className="terlaris-card-container">
+        <XyzTransitionGroup
+          appearVisible
+          xyz="fade up-100% stagger-1 ease-out-back"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-between gap-8 lg:gap-x-24"
+        >
+          <div>
+            <ProductItemCard
+              productName="Indomie Goreng"
+              thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+1"
+              price={3500}
+            />
+          </div>
+
+          <div>
+            <ProductItemCard
+              productName="Indomie Goreng"
+              thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+2"
+              price={3500}
+            />
+          </div>
+
+          <div>
+            <ProductItemCard
+              productName="Indomie Goreng"
+              thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+3"
+              price={3500}
+            />
+          </div>
+
+          <div>
+            <ProductItemCard
+              productName="Indomie Goreng"
+              thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+4"
+              price={3500}
+            />
+          </div>
+        </XyzTransitionGroup>
+      </div>
+    </section>
+  );
+};
+
+export default BestSellerSection;
