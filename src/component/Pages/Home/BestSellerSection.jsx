72ddc6c25e5719f9e94628ca7c46268a34d00e84
Rivaldi <rvdans002@gmail.com>
2022-04-22 08:32:45 +0700

    add id property to BestSellerSection

--- src/component/Pages/Home/BestSellerSection.jsx
+++ src/component/Pages/Home/BestSellerSection.jsx
@@ -12,10 +12,11 @@ const BestSellerSection = ({ className = '' }) => {
         <XyzTransitionGroup
           appearVisible
           xyz="fade up-100% stagger-1 ease-out-back"
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-between gap-8 lg:gap-x-24"
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between gap-8 lg:gap-x-24"
         >
           <div>
             <ProductItemCard
+              id={1}
               productName="Indomie Goreng"
               thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+1"
               price={3500}
@@ -24,6 +25,7 @@ const BestSellerSection = ({ className = '' }) => {
 
           <div>
             <ProductItemCard
+              id={1}
               productName="Indomie Goreng"
               thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+2"
               price={3500}
@@ -32,6 +34,7 @@ const BestSellerSection = ({ className = '' }) => {
 
           <div>
             <ProductItemCard
+              id={1}
               productName="Indomie Goreng"
               thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+3"
               price={3500}
@@ -40,6 +43,7 @@ const BestSellerSection = ({ className = '' }) => {
 
           <div>
             <ProductItemCard
+              id={1}
               productName="Indomie Goreng"
               thumbnailUrl="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+4"
               price={3500}
