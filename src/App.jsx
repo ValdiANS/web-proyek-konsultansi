4c345e8ef05cfb0cda28b40544444f9c552cca15
Rivaldi <rvdans002@gmail.com>
2022-04-22 08:28:29 +0700

    update App commponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Fragment } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 import AppHeader from './component/Layout/AppHeader';
 import AppFooter from './component/Layout/AppFooter';
 import Navbar from './component/Nav/Navbar';
 import Home from './component/Pages/Home/Home';
+import Categories from './component/Pages/Categories/Categories';
+import ProductDetail from './component/Pages/ProductDetail/ProductDetail';
+import Cart from './component/Pages/Cart/Cart';
 
 const App = () => {
   return (
@@ -12,7 +16,14 @@ const App = () => {
         <Navbar />
       </AppHeader>
 
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/categories/:category" element={<Categories />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+      </Routes>
+
+      <Outlet />
 
       <AppFooter></AppFooter>
     </Fragment>
