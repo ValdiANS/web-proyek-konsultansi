d7925590885e34c8e91cf6307844fcff8b5e9e4a
Rivaldi <rvdans002@gmail.com>
2022-04-24 16:33:51 +0700

    adjust logic

--- src/component/Pages/Cart/CartItem.jsx
+++ src/component/Pages/Cart/CartItem.jsx
@@ -4,11 +4,20 @@ import Card from '../../UI/Card';
 import TrashIcon from '../../SVG/TrashIcon';
 import AmountControl from '../../ProductItemCard/AmountControl';
 
-const CartItem = ({}) => {
-  const [itemAmount, setItemAmount] = useState(1);
+const CartItem = ({
+  onCheck = () => {},
+  onDelete = (name = '') => {},
+  thumbnailUrl = '',
+  name = '',
+  brand = '',
+  price = 0,
+  amount = 1,
+  inStock = true,
+  checked = false,
+}) => {
+  const [itemAmount, setItemAmount] = useState(amount);
 
-  const price = itemAmount * 3500;
-  const localPrice = price.toLocaleString('id-ID');
+  const localPrice = (price * itemAmount).toLocaleString('id-ID');
 
   const addAmountHandler = () => {
     setItemAmount((prevVal) => prevVal + 1);
@@ -18,30 +27,49 @@ const CartItem = ({}) => {
     setItemAmount((prevVal) => (prevVal === 1 ? prevVal : prevVal - 1));
   };
 
+  const checkboxClickHandler = () => {
+    onCheck({
+      thumbnailUrl,
+      name,
+      brand,
+      price,
+      amount,
+      inStock,
+    });
+  };
+
+  const deleteClickHandler = () => {
+    onDelete(name);
+  };
+
   return (
     <Card className="border border-solid border-black p-3 flex flex-row gap-x-8">
       <div className="flex flex-col justify-between">
-        <input type="checkbox" name="check" className="w-6 h-6" />
+        <input
+          type="checkbox"
+          name="check"
+          className="w-6 h-6"
+          onChange={checkboxClickHandler}
+          checked={checked}
+        />
 
-        <button>
+        <button onClick={deleteClickHandler}>
           <TrashIcon />
         </button>
       </div>
 
       <Card className="border border-solid border-black p-4 max-w-[14rem]">
-        <img
-          src="https://dummyimage.com/1280x862/e5e5e5/FFFFFF.png&text=Placeholder+Terlaris+1"
-          alt="Indomie Goreng"
-          className="rounded-t-10px w-full"
-        />
+        <img src={thumbnailUrl} alt={name} className="rounded-t-10px w-full" />
       </Card>
 
       <div className="flex flex-col justify-between w-full">
         <div>
-          <div className="bg-[#55E322] px-4 py-1 w-fit">Ada stok</div>
+          <div className="bg-[#55E322] px-4 py-1 w-fit">
+            {inStock ? 'Ada' : 'Tidak ada'} stok
+          </div>
           <div>
-            <h1 className="font-bold">Indomie Goreng</h1>
-            <p>Indofood</p>
+            <h1 className="font-bold">{name}</h1>
+            <p>{brand}</p>
           </div>
         </div>
 
