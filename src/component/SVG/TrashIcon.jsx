6508ed5ea314feb20cace4aa5ad8a83c2ed260ba
Rivaldi <rvdans002@gmail.com>
2022-04-24 16:32:16 +0700

    fix bug

--- src/component/SVG/TrashIcon.jsx
+++ src/component/SVG/TrashIcon.jsx
@@ -11,30 +11,30 @@ const TrashIcon = ({ className = '' }) => {
       <path
         d="M1 6.2H3.6H24.4"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.1001 12.7V20.5"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M15.3 12.7V20.5"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.49985 6.2V3.6C7.49985 2.91044 7.77378 2.24912 8.26138 1.76152C8.74897 1.27393 9.41029 1 10.0999 1H15.2999C15.9894 1 16.6507 1.27393 17.1383 1.76152C17.6259 2.24912 17.8999 2.91044 17.8999 3.6V6.2M21.7999 6.2V24.4C21.7999 25.0896 21.5259 25.7509 21.0383 26.2385C20.5507 26.7261 19.8894 27 19.1999 27H6.19985C5.51029 27 4.84897 26.7261 4.36138 26.2385C3.87378 25.7509 3.59985 25.0896 3.59985 24.4V6.2H21.7999Z"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
