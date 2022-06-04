36f6266f31a6cfe644b0d26cc5ec289630bd418f
frxcksz <92621696+frxcksz@users.noreply.github.com>
2022-06-04 10:22:32 +0700

    Toko Hijrah fix bug routes

--- server/routes/feRoutes.js
+++ server/routes/feRoutes.js
@@ -6,34 +6,34 @@ router.get('', (req, res) =>{
     res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('cart', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('checkout', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('categories/:category', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('products/:productId', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('login', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('forget-password', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('register', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('search', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('admin', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 router.get('dashboard', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client', 'dist', 'index.html'));
 })
 
 module.exports = router
