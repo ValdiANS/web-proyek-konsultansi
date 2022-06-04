ba4e6b7c626cfbdba9c5ed33309536994d657743
frxcksz <92621696+frxcksz@users.noreply.github.com>
2022-06-04 10:07:32 +0700

    Toko Hijrah fix bug routes

--- server/server.js
+++ server/server.js
@@ -42,10 +42,10 @@ routes(app); //register the route*/
 
 if(process.env.NODE_ENV === 'production'){
   console.log('ENV: Production');
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static('../client/dist'));
   
   app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   })
 } else {
   console.log('ENV: Development');
