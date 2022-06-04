6445c0c3b6d9b23620c298f51c8b001be99079c4
frxcksz <92621696+frxcksz@users.noreply.github.com>
2022-06-04 10:04:16 +0700

    Toko Hijrah fix bug routes

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV === 'production'){
   console.log('ENV: Production');
   app.use(express.static(path.join(__dirname, '../client/dist')));
   
-  app.get('/', (req, res) =>{
+  app.get('*', (req, res) =>{
     res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
   })
 } else {
